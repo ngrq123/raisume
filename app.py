19b0df8fa2401c6e93c1d207cc4541e5612bd55f
Rui Qin Ng <ngruiqin_1994@hotmail.com>
2024-06-21 21:57:35 +0800

    Add system prompt and force response in json format

--- app.py
+++ app.py
@@ -14,26 +14,33 @@ chat_client = AzureOpenAI(
   api_version=os.getenv('API_VERSION')
 )
 
+_file = open('.\prompt_templates\system_prompt.txt', 'r', encoding='utf-8')
+system_prompt = _file.read()
+_file.close()
+
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    st.session_state.messages.append({"role": "system", "content": system_prompt})
     st.session_state.messages.append({"role": "assistant", "content": 'Hello 👋'})
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    with st.chat_message(message["role"] if message['role'] != 'system' else 'assistant'):
         st.markdown(message["content"])
 
 prompt = st.chat_input("Say something")
 if prompt:
     with st.chat_message('user'):
-        st.write(prompt)
+        st.markdown(prompt)
     # Add message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Generate response and save
     with st.chat_message('assistant'):
-        response_stream = chat_client.chat.completions.create(model=os.getenv('MODEL_NAME'), 
-                                                              messages=[{'role': m['role'], 'content': m['content']} for m in st.session_state.messages], 
-                                                              stream=True)
-        response_message = st.write_stream(response_stream)
+        response = chat_client.chat.completions.create(messages=[{'role': m['role'], 'content': m['content']} for m in st.session_state.messages], 
+                                                       model=os.getenv('MODEL_NAME'), 
+                                                       response_format={'type': 'json_object'},
+                                                       stream=False)
+        response_message = response.choices[0].message.content
+        st.json(response_message)
     st.session_state.messages.append({"role": "assistant", "content": response_message})
